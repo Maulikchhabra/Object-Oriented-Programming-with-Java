3547d17f4cc3b179aa0b53ce241457eab2539f61
Maulikchhabra <46771434+Maulikchhabra@users.noreply.github.com>
2020-05-09 17:55:12 +0530

    Hackerrank Java Exception Handling problem

---
+++ JavaExceptionHandling.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class JavaExceptionHandling {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        try{
+           int a=sc.nextInt();
+           int b = sc.nextInt();
+           
+           int c = a/b;
+           System.out.println(c);
+           
+           }catch(InputMismatchException e){
+               System.out.println(e.getClass().getName());
+           
+           }catch(ArithmeticException f){
+              System.out.println(f.getClass().getName()+": / by zero");
+           }
+    }
+}
