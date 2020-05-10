82e2543eca0c5af2e5728c1e998055276f9722b5
Maulikchhabra <46771434+Maulikchhabra@users.noreply.github.com>
2020-05-10 18:53:45 +0530

    Hackerrank Java Map problem

---
+++ JavaMap.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.io.*;
+
+class Solution{
+	public static void main(String []argh)
+	{
+		Scanner in = new Scanner(System.in);
+		int n=in.nextInt();
+		in.nextLine();
+
+     Map<String,Integer> m = new HashMap<String,Integer>(n);
+     
+		for(int i=0;i<n;i++)
+		{
+			String name=in.nextLine();
+			int phone=in.nextInt();
+      m.put(name,phone); //inserting values
+			in.nextLine();
+		}
+        
+        //System.out.println(m);
+		
+    while(in.hasNext())
+		{
+			String s=in.nextLine();
+
+            if(m.containsKey(s)){
+               System.out.println(s+"="+m.get(s)); //getting output
+            
+            }else{
+                System.out.println("Not found");
+            }
+		}
+	}
+}
+
+
+
